68345494cffec7d4319fc7305c3072a0700be96e
Antonio Alexandru Ganea <antonio.ganea@yahoo.com>
2019-11-23 13:53:18 +0200

    Fileserver patch

--- fileserver.js
+++ fileserver.js
@@ -4,6 +4,6 @@ const port = 80
 
 app.get('/helloworld', (req, res) => res.send('Hello World!'))
 
-app.use(express.static('public'))
+app.use(express.static('./'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
