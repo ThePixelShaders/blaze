afc1297b977c7135313e6f055c1a1d0597f62629
vlod <vladalex420@gmail.com>
2019-11-23 19:32:48 +0200

    cannon

--- js/client/controller.js
+++ js/client/controller.js
@@ -65,8 +65,8 @@ function onDocumentMouseDown( event ) {
 				socket.emit( "placeTotem", tX, tZ, TotemTypes.residential );
 			}*/
 			
-			SceneManager.addTotem( tX, tZ, TotemTypes.lumber1 );
-			socket.emit( "placeTotem", tX, tZ, TotemTypes.lumber1);
+			SceneManager.addTotem( tX, tZ, TotemTypes.cannon );
+			socket.emit( "placeTotem", tX, tZ, TotemTypes.cannon);
 		}
 	}
 }
