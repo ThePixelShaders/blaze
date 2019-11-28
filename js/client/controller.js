92c6b0ae735ec829eb28a648ffa917868ca72595
Antonio Alexandru Ganea <antonio.ganea@yahoo.com>
2019-11-28 15:03:52 +0200

    Moved hotbar code into hotbar.js

--- js/client/controller.js
+++ js/client/controller.js
@@ -1,13 +1,3 @@
-var HotBar = {
-	currentActive : 1,
-	hotbarMapping : [ TotemTypes.empty, TotemTypes.house1, TotemTypes.lumber1, TotemTypes.mine, TotemTypes.petrol, TotemTypes.nuclearplant, TotemTypes.cannon, TotemTypes.tower, TotemTypes.sappling ],
-
-	// this returns a TotemType
-	getCurrentActive : function(){
-		return this.hotbarMapping [ this.currentActive ];
-	}
-}
-
 var lastCheckX = null
 var lastCheckY = null
 
@@ -461,6 +451,8 @@ function onDocumentKeyDown( event ) {
 
 	if ( CONTROLLER_DISABLED ) { return; }
 
+	if ( HotBar.onDocumentKeyDown( event ) ){ return; }
+
 	switch ( event.keyCode ) {
 		case 16: isShiftDown = true; break;
 
@@ -490,55 +482,6 @@ function onDocumentKeyDown( event ) {
 		break;
 		case 70: /*F*/ this.moveDown = true;
 		//scoreboard.addScore("ion", 69); 
-		break;
-
-		case 49: /*1*/ 
-			$("li.hotbar-box-active").removeClass("hotbar-box-active");
-			$("li#hotbar-box1").addClass("hotbar-box-active");
-			HotBar.currentActive = 1;
-			itemDetails(HotBar.currentActive);
-		break;
-		case 50: /*2*/ 
-			$("li.hotbar-box-active").removeClass("hotbar-box-active");
-			$("li#hotbar-box2").addClass("hotbar-box-active");
-			HotBar.currentActive = 2;
-			itemDetails(HotBar.currentActive);
-		break;
-		case 51: /*3*/ 
-			$("li.hotbar-box-active").removeClass("hotbar-box-active");
-			$("li#hotbar-box3").addClass("hotbar-box-active");
-			HotBar.currentActive = 3;
-			itemDetails(HotBar.currentActive);
-		break;
-		case 52: /*4*/ 
-			$("li.hotbar-box-active").removeClass("hotbar-box-active");
-			$("li#hotbar-box4").addClass("hotbar-box-active");
-			HotBar.currentActive = 4;
-			itemDetails(HotBar.currentActive);
-		break;
-		case 53: /*5*/ 
-			$("li.hotbar-box-active").removeClass("hotbar-box-active");
-			$("li#hotbar-box5").addClass("hotbar-box-active");
-			HotBar.currentActive = 5;
-			itemDetails(HotBar.currentActive);
-		break;
-		case 54: /*6*/ 
-			$("li.hotbar-box-active").removeClass("hotbar-box-active");
-			$("li#hotbar-box6").addClass("hotbar-box-active");
-			HotBar.currentActive = 6;
-			itemDetails(HotBar.currentActive);
-		break;
-		case 55: /*7*/ 
-			$("li.hotbar-box-active").removeClass("hotbar-box-active");
-			$("li#hotbar-box7").addClass("hotbar-box-active");
-			HotBar.currentActive = 7;
-			itemDetails(HotBar.currentActive);
-		break;
-		case 56: /*8*/ 
-			$("li.hotbar-box-active").removeClass("hotbar-box-active");
-			$("li#hotbar-box8").addClass("hotbar-box-active");
-			HotBar.currentActive = 8;
-			itemDetails(HotBar.currentActive);
 		break;
 	}
 
@@ -579,27 +522,7 @@ function onDocumentKeyUp( event ) {
 }
 
 function onDocumentWheel ( event ){
-	if(event.deltaY>0){
-		HotBar.currentActive++;
-		if(HotBar.currentActive == 9)
-		{
-			HotBar.currentActive = 1;
-		}
-		$("li.hotbar-box-active").removeClass("hotbar-box-active");
-		$("li#hotbar-box" + HotBar.currentActive).addClass("hotbar-box-active");
-		itemDetails(HotBar.currentActive);
-	}
-	else{ 
-		HotBar.currentActive--;
-		if(HotBar.currentActive == 0)
-		{
-			HotBar.currentActive = 8;
-		}
-		$("li.hotbar-box-active").removeClass("hotbar-box-active");
-		$("li#hotbar-box" + HotBar.currentActive).addClass("hotbar-box-active");
-		itemDetails(HotBar.currentActive);
-	}
-	//console.log(HotBar.currentActive);
+	HotBar.onDocumentWheel( event )
 }
 
 var spectateMode = true;
